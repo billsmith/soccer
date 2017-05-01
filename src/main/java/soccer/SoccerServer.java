6b4c6b39af90ead7b1bbeb4bad4cd08c95d47bd2
Bill Smith <billsmithaustin@yahoo.com>
2017-04-30 20:03:28 -0500

    Add authentication

--- src/main/java/soccer/SoccerServer.java
+++ src/main/java/soccer/SoccerServer.java
@@ -1,16 +1,55 @@
 package soccer;
 
+import org.eclipse.jetty.security.*;
+import org.eclipse.jetty.security.authentication.BasicAuthenticator;
 import org.eclipse.jetty.server.Handler;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.handler.ContextHandler;
 import org.eclipse.jetty.server.handler.ContextHandlerCollection;
 import org.eclipse.jetty.server.handler.ResourceHandler;
+import org.eclipse.jetty.util.security.Constraint;
 
 import java.io.File;
 import java.io.FileReader;
+import java.util.Collections;
 import java.util.Properties;
 
 public class SoccerServer {
+    private static final String REALM = "soccer";
+
+    private static ConstraintSecurityHandler getSecurityHandler() {
+        // add authentication
+        Constraint constraint = new Constraint(Constraint.__BASIC_AUTH,"user");
+        constraint.setAuthenticate(true);
+        constraint.setRoles(new String[]{"user"});
+
+        // map the security constraint to the root path.
+        ConstraintMapping cm = new ConstraintMapping();
+        cm.setConstraint(constraint);
+        cm.setPathSpec("/*");
+
+        // create the security handler, set the authentication to Basic
+        // and assign the realm.
+        ConstraintSecurityHandler csh = new ConstraintSecurityHandler();
+        csh.setAuthenticator(new BasicAuthenticator());
+        csh.setRealmName(REALM);
+        csh.addConstraintMapping(cm);
+
+        // set the login service
+        csh.setLoginService(getHashLoginService());
+
+        return csh;
+
+    }
+    private static HashLoginService getHashLoginService() {
+
+        // create the login service, assign the realm and read the user credentials
+        // from the file /tmp/realm.properties.
+        HashLoginService hls = new HashLoginService();
+        hls.setName(REALM);
+        hls.setConfig("realm.properties");
+        return hls;
+    }
 
     public static void main( String[] args ) throws Exception {
         final File propertiesFile = new File(args[0]);
@@ -19,11 +58,29 @@ public class SoccerServer {
             System.exit(1);
         }
 
-        Server server = new Server(8080);
+        final Server server = new Server(8080);
 
-        ContextHandler context = new ContextHandler("/hello");
-        context.setContextPath("/hello");
-        context.setHandler(new HelloHandler());
+        final LoginService loginService = getHashLoginService();
+        server.addBean(loginService);
+
+        final ConstraintSecurityHandler security = getSecurityHandler();
+
+        Constraint constraint = new Constraint();
+        constraint.setName("auth");
+        constraint.setAuthenticate(true);
+        constraint.setRoles(new String[] { Constraint.ANY_AUTH });
+
+        ConstraintMapping mapping = new ConstraintMapping();
+        mapping.setPathSpec("/*");
+        mapping.setConstraint(constraint);
+
+        security.setConstraintMappings(Collections.singletonList(mapping));
+        security.setAuthenticator(new BasicAuthenticator());
+        security.setLoginService(loginService);
+
+        ContextHandler contextHello = new ContextHandler("/hello");
+        contextHello.setContextPath("/hello");
+        contextHello.setHandler(new HelloHandler());
 
         final Properties properties = new Properties();
         properties.load(new FileReader(propertiesFile));
@@ -47,8 +104,10 @@ public class SoccerServer {
         contextUI.setHandler(resourceHandler);
 
         ContextHandlerCollection contexts = new ContextHandlerCollection();
-        contexts.setHandlers(new Handler[] { context, contextUI, contextGo });
-        server.setHandler(contexts);
+        contexts.setHandlers(new Handler[] { contextHello, contextUI, contextGo });
+        security.setHandler(contexts);
+
+        server.setHandler(security);
 
         server.start();
         server.join();
